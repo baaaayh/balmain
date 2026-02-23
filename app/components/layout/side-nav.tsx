39be5b3fb9cb876670b82084a153b4e938998998
WIN11PRO\WIN11PRO <baaaayh@gmail.com>
2026-02-24 01:29:09 +0900

    Updated.

--- app/components/layout/side-nav.tsx
+++ app/components/layout/side-nav.tsx
@@ -10,92 +10,81 @@ import clsx from "clsx";
 import styles from "@/app/styles/layout/side-nav.module.scss";
 
 export default memo(function SideNav() {
-    const { isOpen, actions } = useSideNavStore((state) => state);
-    const pathname = usePathname();
-    const [isExpended, setIsExpended] = useState(false);
-    const [windowWidth, setWindowWidth] = useState<number>(0);
+  const { isOpen, actions } = useSideNavStore((state) => state);
+  const pathname = usePathname();
+  const [isExpended, setIsExpended] = useState(false);
+  const [windowWidth, setWindowWidth] = useState<number>(0);
 
-    const handleNavState = useCallback(() => {
-        actions.toggleSideNav();
-    }, [actions]);
+  const handleNavState = useCallback(() => {
+    actions.toggleSideNav();
+  }, [actions]);
 
-    const handleNavClose = useCallback(() => {
-        actions.closeSideNav();
-    }, [actions]);
+  const handleNavClose = useCallback(() => {
+    actions.closeSideNav();
+  }, [actions]);
 
-    const handleTransitionEnd = useCallback(() => {
-        if (isOpen) {
-            setIsExpended(true);
-        } else {
-            setIsExpended(false);
-        }
-    }, [isOpen]);
+  const handleTransitionEnd = useCallback(() => {
+    if (isOpen) {
+      setIsExpended(true);
+    } else {
+      setIsExpended(false);
+    }
+  }, [isOpen]);
 
-    useEffect(() => {
-        actions.closeSideNav();
-    }, [pathname, handleNavClose, actions]);
+  useEffect(() => {
+    actions.closeSideNav();
+  }, [pathname, handleNavClose, actions]);
 
-    useEffect(() => {
-        function calcWindowWidth() {
-            const ww = window.innerWidth;
-            setWindowWidth(ww);
-        }
-        calcWindowWidth();
-        window.addEventListener("resize", calcWindowWidth);
-    }, []);
+  useEffect(() => {
+    function calcWindowWidth() {
+      const ww = window.innerWidth;
+      setWindowWidth(ww);
+    }
+    calcWindowWidth();
+    window.addEventListener("resize", calcWindowWidth);
+  }, []);
 
-    useEffect(() => {
-        if (isOpen && windowWidth <= 900) {
-            document.body.style.overflow = "hidden";
-        } else {
-            document.body.style.overflow = "";
-        }
-    }, [windowWidth, isOpen]);
+  useEffect(() => {
+    if (isOpen && windowWidth <= 900) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+  }, [windowWidth, isOpen]);
 
-    return (
-        <MenuProvider>
-            {
-                <nav
-                    className={clsx(styles["side-nav"], {
-                        [styles["side-nav--active"]]: isOpen,
-                    })}
-                    onTransitionEnd={handleTransitionEnd}
+  return (
+    <MenuProvider>
+      {
+        <nav
+          className={clsx(styles["side-nav"], {
+            [styles["side-nav--active"]]: isOpen,
+          })}
+          onTransitionEnd={handleTransitionEnd}
+        >
+          <div
+            className={clsx(styles["side-nav__inner"], {
+              [styles["side-nav__inner--active"]]: isExpended,
+            })}
+          >
+            <div className={clsx(styles["side-nav__contents"])}>
+              <div className={clsx(styles["side-nav__header"])}>
+                <button
+                  type="button"
+                  className={clsx("btn btn-menu", {
+                    ["btn-menu--active"]: isExpended,
+                  })}
+                  onClick={handleNavState}
                 >
-                    <div
-                        className={clsx(styles["side-nav__inner"], {
-                            [styles["side-nav__inner--active"]]: isExpended,
-                        })}
-                    >
-                        <div className={clsx(styles["side-nav__contents"])}>
-                            <div className={clsx(styles["side-nav__header"])}>
-                                <button
-                                    type="button"
-                                    className={clsx("btn btn-menu", {
-                                        ["btn-menu--active"]: isExpended,
-                                    })}
-                                    onClick={handleNavState}
-                                >
-                                    <span className="btn btn-icon__text">
-                                        MENU
-                                    </span>
-                                </button>
-                            </div>
-                            <SideNavUtils
-                                navState={isOpen}
-                                isExpended={isExpended}
-                            />
-                            <SideNavMenu
-                                navState={isOpen}
-                                isExpended={isExpended}
-                            />
-                        </div>
-                        <SideNavFooter
-                            navState={isOpen}
-                            isExpended={isExpended}
-                        />
-                    </div>
-                </nav>
-            }
-        </MenuProvider>
-    );
+                  <span className="btn btn-icon__text">MENU</span>
+                </button>
+              </div>
+              <SideNavUtils navState={isOpen} isExpended={isExpended} />
+              <SideNavMenu navState={isOpen} isExpended={isExpended} />
+            </div>
+            <SideNavFooter navState={isOpen} isExpended={isExpended} />
+          </div>
+        </nav>
+      }
+    </MenuProvider>
+  );
 });
--- type.d.ts
+++ type.d.ts
@@ -1,120 +1,120 @@
 export interface LinkButtonType {
-    buttonText: string;
-    linkString: string;
-    buttonColorClass: string;
+  buttonText: string;
+  linkString: string;
+  buttonColorClass: string;
 }
 
 export interface CardSectionDataType {
-    imageUrl_desktop: string;
-    imageUrl_mobile: string;
-    title: string;
-    link: string;
-    buttons: LinkButtonType[];
+  imageUrl_desktop: string;
+  imageUrl_mobile: string;
+  title: string;
+  link: string;
+  buttons: LinkButtonType[];
 }
 
 export interface CircleDataType {
-    imageUrl: string;
-    title: string;
+  imageUrl: string;
+  title: string;
 }
 
 export interface ReinsuranceDataType extends LinkButtonType {
-    title: string;
-    paragraph: string;
+  title: string;
+  paragraph: string;
 }
 
 export interface MenuDataType {
-    menu_id: number;
-    parent_menu_id: number | null;
-    depth1: string;
-    depth2: string;
-    depth3: string;
-    depth4: string;
-    depth_level: number;
-    path: string;
-    border: boolean | null;
-    link: string | null;
-    prev_path: string | null;
-    prev_menu_id: number | null;
-    prev_menu: string | null;
+  menu_id: number;
+  parent_menu_id: number | null;
+  depth1: string;
+  depth2: string;
+  depth3: string;
+  depth4: string;
+  depth_level: number;
+  path: string;
+  border: boolean | null;
+  link: string | null;
+  prev_path: string | null;
+  prev_menu_id: number | null;
+  prev_menu: string | null;
 }
 
 export interface SideNavMenuProps {
-    expandedDepth2: number | null;
-    expandedDepth3: number | null;
-    setExpandedDepth2: (
-        value: number | null | ((prev: number | null) => number | null)
-    ) => void;
-    setExpandedDepth3: (
-        value: number | null | ((prev: number | null) => number | null)
-    ) => void;
-    depth1: {
-        menu_id: number;
-        depth1: string;
-        border: boolean | null;
-        link: string | null;
-    };
-    depth2Menus: Array<{
-        menu_id: number;
-        depth2: string;
-        border: boolean | null;
-        path: string | null;
-        link: string | null;
-    }>;
-    menus: Array<menuDataType>;
-    index: number;
-    path: string;
+  expandedDepth2: number | null;
+  expandedDepth3: number | null;
+  setExpandedDepth2: (
+    value: number | null | ((prev: number | null) => number | null),
+  ) => void;
+  setExpandedDepth3: (
+    value: number | null | ((prev: number | null) => number | null),
+  ) => void;
+  depth1: {
+    menu_id: number;
+    depth1: string;
+    border: boolean | null;
+    link: string | null;
+  };
+  depth2Menus: Array<{
+    menu_id: number;
+    depth2: string;
+    border: boolean | null;
+    path: string | null;
+    link: string | null;
+  }>;
+  menus: Array<menuDataType>;
+  index: number;
+  path: string;
 }
 
 export interface ProductDataType {
-    product_id: number;
-    base_item_code: string;
-    item_code: string;
-    name: string;
-    category1: string | null;
-    category1_1: string | null;
-    category1_3: string | null;
-    category1_4: string | null;
-    collections1: string | null;
-    collections2: string | null;
-    collections3: string | null;
-    collections4: string | null;
-    menu_id: number;
-    category_id: number;
-    price: number;
-    image_alt: string;
-    image_filenames: Array<string>;
-    color: string;
+  product_id: number;
+  base_item_code: string;
+  item_code: string;
+  name: string;
+  category1: string | null;
+  category1_1: string | null;
+  category1_3: string | null;
+  category1_4: string | null;
+  collections1: string | null;
+  collections2: string | null;
+  collections3: string | null;
+  collections4: string | null;
+  menu_id: number;
+  category_id: number;
+  price: number;
+  image_alt: string;
+  image_filenames: Array<string>;
+  color: string;
 }
 
 export interface ProductDetailDataType extends ProductDataType {
-    sizes: Array<string>;
-    colors: Array<{
-        id: number;
-        name: string;
-    }>;
-    description_contents: string;
+  sizes: Array<string>;
+  colors: Array<{
+    id: number;
+    name: string;
+  }>;
+  description_contents: string;
 }
 
 export interface CartProductDataType {
-    product_id: number;
-    item_code: string;
-    menu_id: number;
+  product_id: number;
+  item_code: string;
+  menu_id: number;
+  name: string;
+  price: number;
+  quantity: number;
+  selectedSize: string;
+  selectedColor: {
+    id: string;
     name: string;
-    price: number;
-    quantity: number;
-    selectedSize: string;
-    selectedColor: {
-        id: string;
-        name: string;
-    };
-    thumbUrl: string;
+  };
+  thumbUrl: string;
 }
 
 export interface ChangedStateType {
-    productId: number | null;
-    selectedColor: {
-        id: string;
-        name: string;
-    } | null;
-    selectedSize: string;
+  productId: number | null;
+  selectedColor: {
+    id: string;
+    name: string;
+  } | null;
+  selectedSize: string;
 }
